4f7ab5a970500aa3b749a4049402519fdf7a0920
Harry Wixley <hwixley1@gmail.com>
2023-04-10 21:32:50 +0100

    moved formatters to responseHandler

--- src/chat_bot.py
+++ src/chat_bot.py
@@ -23,30 +23,9 @@ class Main():
             print(f"\nResponse:\n{response}")
             sys.exit()
         else:
-            self.save_code_blocks(code_blocks, responseHandler)
+            responseHandler.save_code_blocks(code_blocks, responseHandler)
+            print(f"\nResponse:\n{response}")
 
-    def save_code_blocks(self, code_blocks, responseHandler):
-        print("\nWhat would you like to call your script? (exclude the file extension)")
-        fname = input()
-
-        num_args = len(responseHandler.findArgs())
-        print(f"\nThis script has {num_args} arguments.")
-
-        dirpath = f'generated-scripts/{fname}'
-        os.system(dirpath)
-
-        file_paths = []
-        for i, code_block in enumerate(code_blocks):
-            file_paths += [f'{dirpath}/{fname}-{i}.py']
-            self.save_file(file_paths[-1], code_block[1])
-
-        self.save_file(f'{dirpath}/{fname}.sh', f"python3 {file_paths[0]} $@")
-
-    def save_file(self, file_path, file_content):
-        with open(file_path, 'w') as f:
-            f.write(file_content)
-            if file_path.endswith(".sh"):
-                os.system(f'chmod +x {file_path}')
 
 if __name__ == "__main__":
     main = Main()
--- src/response_handler.py
+++ src/response_handler.py
@@ -1,4 +1,4 @@
-import re
+import re, os
 
 class ResponseHandler():
         
@@ -12,3 +12,26 @@ class ResponseHandler():
     def findArgs(self):
         args_regex = "arg[0-9]"
         return re.findall(args_regex, self.response)
+
+    def save_code_blocks(self, code_blocks):
+        print("\nWhat would you like to call your script? (exclude the file extension)")
+        fname = input()
+
+        num_args = len(self.findArgs())
+        print(f"\nThis script has {num_args} arguments.")
+
+        dirpath = f'generated-scripts/{fname}'
+        os.system(dirpath)
+
+        file_paths = []
+        for i, code_block in enumerate(code_blocks):
+            file_paths += [f'{dirpath}/{fname}-{i}.py']
+            self.save_file(file_paths[-1], code_block[1])
+
+        self.save_file(f'{dirpath}/{fname}.sh', f"python3 {file_paths[0]} $@")
+
+    def save_file(self, file_path, file_content):
+        with open(file_path, 'w') as f:
+            f.write(file_content)
+            if file_path.endswith(".sh"):
+                os.system(f'chmod +x {file_path}')
