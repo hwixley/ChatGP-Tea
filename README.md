c88cb8760d6cca8e4637509c9e544910b8ceea5c
Harry Wixley <hwixley1@gmail.com>
2023-04-22 14:55:15 +0100

    setup better prompts, colours, and constants

--- README.md
+++ README.md
@@ -1,25 +1,30 @@
 # ChatGP-Tea CLI ☕️
-A CLI for talking to ChatGPT, saving conversation logs, and turning code blocks (language agnostic) from it's responses into executable shell commands.
+A CLI for talking to ChatGPT:, saving conversation logs, and turning code blocks (language agnostic) from it's responses into executable shell commands.
 
 ## Setup
 
-1. Create an API key on the OpenAI developer portal, and run the following command:
+1. Install the python dependencies:
+    ```
+    pip install -r requirements.txt
+    ```
+
+2. Create an API key on the OpenAI developer portal, and run the following command:
     ```
     export OPENAI_API_KEY=<your-api-key>
     ```
 
-2. Run the setup script. This will add the `gptea` alias for your script in `.bashrc` for easy execution in any directory, and add local directories (`generated-scripts/` and `conversations/`) for saving ChatGPT generated files or conversations.
+3. Run the setup script. This will add the `gptea` alias for your script in `.bashrc` for easy execution in any directory, and add local directories (`generated-scripts/` and `conversations/`) for saving ChatGPT: generated files or conversations.
    ```
    chmod +x setup.sh && source setup.sh
    ```
 
-3. You are good to go! 🤠 Run `gptea` to get started
+4. You are good to go! 🤠 Run `gptea` to get started
 
 ## List of commands
 
-1. _ : no input will default to starting a continuous conversation with ChatGPT.
-2. `ask` : ask ChatGPT a question without saving any files.
-3. `convo` : have a continuous conversation with ChatGPT.
-4. `lang <lang-name>` : ask ChatGPT to create a bash script that uses the input language (`<lang-name>`) under the hood.<br>
+1. _ : no input will display the list of commands.
+2. `ask` : ask ChatGPT: a question without saving any files.
+3. `convo` : have a continuous conversation with ChatGPT:.
+4. `lang <lang-name>` : ask ChatGPT: to create a bash script that uses the input language (`<lang-name>`) under the hood.<br>
 **Available `<lang-name>` values:**<br>
 `python`, `bash`, `javascript`, `typescript`, `java`, `c`, `c++`, `c#`, `rust`, `swift`, `go`, `ruby`, `php`
--- src/chat_bot.py
+++ src/chat_bot.py
@@ -1,10 +1,32 @@
 import os, sys
 import openai_service, response_handler, languages
+from termcolor import colored
 
 class Main():
 
+    VERSION="0.1.0"
+    KEY_FILE=".env"
+    KEY_NAME="OPENAI_API_KEY"
+
     def __init__(self):
-        API_KEY = os.environ.get("OPENAI_API_KEY")
+        self.message("Welcome to ChatGP-Tea!")
+        self.message("This is a chatbot that uses the OpenAI API to generate responses to your questions.")
+
+        print(colored(f"\nVersion: {self.VERSION}", "magenta"))
+
+        print("\nCommands:")
+        print("- ask    : Ask a question.")
+        print("- lang   : Ask a question in a specific language.")
+        print("- convo  : Start a conversation with the bot.")
+
+        API_KEY = os.environ.get(self.KEY_NAME)
+        if API_KEY == None:
+            if os.path.exists(self.KEY_FILE):
+                with open(self.KEY_FILE, "r") as f:
+                    API_KEY = f.read().replace(f"{self.KEY_NAME}=", "")
+            else:
+                self.error("No API key found.")
+                sys.exit()
         self.openaiService = openai_service.OpenAIService(API_KEY)
 
     def process_args(self):
@@ -18,36 +40,38 @@ class Main():
                 if n > 1:
                     self.get_lang_response(args[1])
                 else:
-                    print("\nERROR: No language specified.")
+                    self.error("No language specified.")
             elif arg0 == "convo":
                 self.start_convo()                
             else:
-                print(f"\nERROR: Invalid argument: {args[0]}")
+                self.error(f"Invalid argument: {args[0]}")
         else:
-            self.start_convo()
+            self.info("Type a command to get started!\n")
 
     def start_convo(self):
-        print("\nWhat would you like to ask?")
+        self.info("User:")
         question = input()
 
         while question != "exit":
             response = self.openaiService.get_response(question)
-            print(f"\nResponse:\n{response}")
-            print("\nWhat would you like to ask?")
+            self.info("ChatGPT:")
+            print(f"{response}")
+            self.info("User:")
             question = input()
 
-        print("Would you like to save this conversation? (y/n)")
+        self.message("\nWould you like to save this conversation? (y/n)")
         save = input()
         if save == "y":
-            print("\nWhat would you like to call this conversation?")
+            self.message("\nWhat would you like to call this conversation?")
             fname = input()
             self.openaiService.save_conversation(fname)
         
-        print("\nGoodbye!")
+        self.message("\nGoodbye!")
 
     def get_response(self):
         response = main.ask_question()
-        print(f"\nResponse:\n{response}")
+        self.info("ChatGPT:")
+        print(f"\n{response}")
 
     def get_lang_response(self, lang):
         lang = languages.Languages().get_language(lang)
@@ -55,16 +79,16 @@ class Main():
             response = main.ask_lang_question(parseLanguage=lang)
             main.format_lang_response(response, lang=lang)
         else:
-            print(f"\nERROR: Invalid language: {lang}")
+            self.error(f"Invalid language: {lang}")
 
     def ask_question(self):
-        print("\nWhat would you like to ask?")
+        self.info("User:")
         question = input()
 
         return self.openaiService.get_response(question)
 
     def ask_lang_question(self, parseLanguage=languages.Languages().get_language("python"), executeLanguage="bash"):
-        print(f"\nWhat would you like your {executeLanguage} command to do?")
+        self.info(f"What would you like your {executeLanguage} command to do?")
         question = input()
 
         return self.openaiService.get_lang_response(question, parseLanguage)
@@ -74,12 +98,23 @@ class Main():
         code_blocks = responseHandler.findCodeBlocks(lang=lang)
 
         if len(code_blocks) == 0:
-            print("\nERROR: No code found in response.")
-            print(f"\nResponse:\n{response}")
+            self.error("No code found in response.")
+            self.info("ChatGPT:")
+            print(f"\n{response}")
             sys.exit()
         else:
             responseHandler.save_code_blocks(code_blocks, lang)
-            print(f"\nResponse:\n{response}")
+            self.info("ChatGPT:")
+            print(f"\n{response}")
+
+    def error(self, msg):
+        print(colored(f"\nERROR: {msg}", "red"))
+
+    def info(self, msg):
+        print(colored(f"\n{msg}", "green"))
+
+    def message(self, msg):
+        print(colored(f"{msg}", "blue"))
 
 
 if __name__ == "__main__":
